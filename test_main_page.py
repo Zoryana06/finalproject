ca066838f661f9242fa50ed3e69b786ad7e21d1b
Zoryana Pokotska <zoryanapokotska@gmail.com>
2019-11-09 22:31:27 +0200

    first test based on Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,7 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)
+	page.open()
+	page.go_to_login_page()
