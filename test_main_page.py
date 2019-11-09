22b9b897bbfa7fdc52410f053a8d4da902acaf24
Zoryana Pokotska <zoryanapokotska@gmail.com>
2019-11-09 23:32:19 +0200

    check element on page with assert

--- test_main_page.py
+++ test_main_page.py
@@ -5,3 +5,9 @@ def test_guest_can_go_to_login_page(browser):
 	page = MainPage(browser, link)
 	page.open()
 	page.go_to_login_page()
+
+def test_guest_can_go_to_login_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)
+	page.open()
+	page.should_be_login_link()
