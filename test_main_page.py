95b5798aaf73c955909f308f2f1a3f8037a3940c
Zoryana Pokotska <zoryanapokotska@gmail.com>
2019-11-10 13:47:09 +0200

    replace 'link' to 'browser.current_url' in LoginPage inizialization

--- test_main_page.py
+++ test_main_page.py
@@ -17,5 +17,5 @@ def test_guest_can_go_to_login_page(browser):
 	page = MainPage(browser, link)
 	page.open()
 	page.go_to_login_page()
-	page = LoginPage(browser, link)
+	page = LoginPage(browser, browser.current_url)
 	page.should_be_login_page()
