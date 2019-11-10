353999742cc4a1b802198f7304ec496170516664
Zoryana Pokotska <zoryanapokotska@gmail.com>
2019-11-10 13:40:45 +0200

    check login url, login form and register form

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,21 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/"
 	page = MainPage(browser, link)
 	page.open()
 	page.go_to_login_page()
 
 def test_guest_can_go_to_login_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/"
 	page = MainPage(browser, link)
 	page.open()
 	page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+	page = MainPage(browser, link)
+	page.open()
+	page.go_to_login_page()
+	page = LoginPage(browser, link)
+	page.should_be_login_page()
